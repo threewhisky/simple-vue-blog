cc93f6d96c27e9042ff519e2db7e0040b7dd15d9
threewhisky <threewhisky@163.com>
2022-03-20 12:27:30 +0800

    实现各组件之间的路由，同时添加单个博客页面。

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,26 @@
 <template>
   <div id="app">
     <!-- <add-blog></add-blog> -->
-    <show-blogs></show-blogs>
+    <!-- <show-blogs></show-blogs> -->
+    <blog-header></blog-header>
+    <router-view></router-view>
+    <!-- <router-link to="/">Home</router-link>
+    <router-link to="/add">About</router-link>   -->
   </div>
 </template>
 
 <script>
 import AddBlog from './components/AddBlog.vue'
 import ShowBlogs from './components/ShowBlogs.vue'
+import BlogHeader from './components/BlogHeader.vue'
+
 
 export default {
   name: 'app',
   components: {
     'add-blog': AddBlog,
     'show-blogs':ShowBlogs,
+    'blog-header': BlogHeader,
   }
 }
 </script>
